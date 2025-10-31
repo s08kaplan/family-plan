8a58aeed8a8dbe7ad4b52db36fa2550ed69c2fee
serkan <s8ersis@gmail.com>
2025-10-31 19:06:05 +0300

    chore: update health parts for diagnoses and medicines with array type

--- backend/src/schema/health-for-kids.ts
+++ backend/src/schema/health-for-kids.ts
@@ -1,13 +1,15 @@
-import { date, integer, pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { date, integer, jsonb, pgTable, text, uuid } from "drizzle-orm/pg-core";
 import { timestamps } from "../helpers/timestamps.js";
 import { users } from "./user.js";
 
 export const kidsHealth = pgTable("kids_health", {
-    id: uuid().primaryKey().defaultRandom(),
-    userId: integer("user_id").notNull().references(() => users.id, { onDelete: "cascade"}),
-    lastHospitalVisit: date("last_hospital_visit").notNull(),
-    departments: text("departments"),
-    diagnoses: text("diagnoses").notNull(),
-    medicines: text("medicines"),
-    ...timestamps
-})
+  id: uuid().primaryKey().defaultRandom(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  lastHospitalVisit: date("last_hospital_visit").notNull(),
+  departments: text("departments"),
+  diagnoses: jsonb("diagnoses").$type<string[]>().notNull().default([]),
+  medicines: jsonb("medicines").$type<string[]>().default([]),
+  ...timestamps,
+});
